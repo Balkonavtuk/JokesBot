350986700fae999bcbaa3a6e636ffda514e9865a
Balkonavtuk <28092005sasha@gmail.com>
2025-05-22 20:51:37 +0300

    second commit

--- bot/src/main/java/Pestryakov/bot/configuration/TelegramBotConfiguration.java
+++ bot/src/main/java/Pestryakov/bot/configuration/TelegramBotConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 @Configuration
 public class TelegramBotConfiguration {
-
     @Bean
     public TelegramBot telegramBot(@Value("${telegram.bot.token}") String token) {
         return new TelegramBot(token);
